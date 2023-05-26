9d650f6798c2af3573892602096139f60d97bcf5
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-26 08:44:19 -0700

    prep Engineer Controller to connect to Views.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -10,10 +10,39 @@ namespace Factory.Controllers
       _db = db;
     }
 
+//READ vvvv
+
     public ActionResult Index()
     {
         return View();
     }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+                                  .Include()...
+
+      return View(thisEngineer);
+    }
+
+//READ ^^^^
+//----------------------------------------------------------------
+// CREATE vvvv
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+    }
+
+// CREATE ^^^^
+//----------------------------------------------------------------
 
 }
 }
