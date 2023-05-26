76e7d9477f36e53e74d544243ddf70c5845fb543
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-26 08:46:25 -0700

    prep Machine Controller to connect to Views.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -10,10 +10,39 @@ namespace Factory.Controllers
       _db = db;
     }
 
+   //READ vvvv
+
     public ActionResult Index()
     {
         return View();
     }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+                                  .Include()...
+
+      return View(thisMachine);
+    }
+
+//READ ^^^^
+//----------------------------------------------------------------
+// CREATE vvvv
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+    }
+
+// CREATE ^^^^
+//----------------------------------------------------------------
 
 }
 }
