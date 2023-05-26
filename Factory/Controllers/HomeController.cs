72d45c7fe35bce2acb30353ce46e40e800a91b48
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-26 15:50:22 -0700

    update Home controller to display lists of engineers and machines.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
-{
+  public class HomeController : Controller
+  {
+
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
-        return View();
+			Engineer[] engs = _db.Engineers.ToArray();
+			Machine[] machs = _db.Machines.ToArray();
+			Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+			model.Add("engineers", engs);
+			model.Add("machines", machs);
+      return View(model);
     }
-
-}
+  }
 }
